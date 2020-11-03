f45bb70154d50941c2417f6060c6237576447a1d
aadilMohamed <72676882+aadilMohamed@users.noreply.github.com>
2020-11-03 16:23:53 +0800

    Update sketch.js

--- boilerplate(extracted)/p5.play-boilerplate-master/sketch.js
+++ boilerplate(extracted)/p5.play-boilerplate-master/sketch.js
@@ -11,20 +11,18 @@ function setup() {
 }
 function draw() {
   background(0,0,0);
-  car.debug = true;
-  wall.debug = true;
-  var deformation = 0.5*speed*speed/22509;
-  if(wall.x-car.x<(car.width+wall.width)/2){
+  var deformation = 0.5 * weight * speed * speed/22509;
+  if(wall.x-car.x<=(car.width+wall.width)/2){
     car.velocityX = 0;
     
     if(deformation>180){
-      car.shapeColor = color("red");
+      car.shapeColor = color(25,0,0);
     }
     if(deformation<180 && deformation>100){
-      car.shapeColor = color("yellow");
+      car.shapeColor = color(230,230,0);
     }
-    if(deformation>100){
-      car.shapeColor = color("green");
+    else if(deformation>100){
+      car.shapeColor = color(0,255,0);
     }
   }
   drawSprites();
