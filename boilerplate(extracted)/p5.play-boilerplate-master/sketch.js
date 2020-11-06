0a0b9da0c3397168b2acc6547a5d773d74a9b38b
aadilMohamed <72676882+aadilMohamed@users.noreply.github.com>
2020-11-06 11:07:51 +0800

    Update sketch.js

--- boilerplate(extracted)/p5.play-boilerplate-master/sketch.js
+++ boilerplate(extracted)/p5.play-boilerplate-master/sketch.js
@@ -12,7 +12,7 @@ function setup() {
 function draw() {
   background(0,0,0);
   var deformation = 0.5 * weight * speed * speed/22509;
-  if(wall.x-car.x<=(car.width+wall.width)/2){
+  if(wall.x-car.x<(car.width+wall.width)/2){
     car.velocityX = 0;
     
     if(deformation>180){
